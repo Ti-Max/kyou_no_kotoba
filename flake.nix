d42ee5e84306026db64795bb72052f316d535428
Timofei Maksimov <maxtimofy@gmail.com>
2025-02-02 22:42:41 +0100

    fix: format + typo

--- flake.nix
+++ flake.nix
@@ -15,17 +15,17 @@
       x86_64LinuxBuildInputs = with x86_64-linux_pkgs; [ elixir_1_18 inotify-tools ];
 
       buildShell = { pkgs, buildInputs }:
-      pkgs.mkShell {
-        buildInputs = buildInputs;
-        shellHook = ''
-          echo "It's time for 今日の言葉!"
-        '';
-      };
+        pkgs.mkShell {
+          buildInputs = buildInputs;
+          shellHook = ''
+            echo "It's time for 今日の言葉!"
+          '';
+        };
     in
     {
-      devShells.aarch64-darwin.default = buildShell {pkgs=aarch64-darwin_pkgs; buildInputs=aarch64DarwinBuildInputs;};
-      devShells.aarch64-linux.default = buildShell { pkgs=aarch64-linux_pkgs; buildInputs=aarch64LinuxBuildInputs;};
-      devShells.x86_64-linux.default = buildShell { pkgs=-x86_64-linux_pkgs; buildInputs=x86_64LinuxBuildInputs;};
+      devShells.aarch64-darwin.default = buildShell { pkgs = aarch64-darwin_pkgs; buildInputs = aarch64DarwinBuildInputs; };
+      devShells.aarch64-linux.default = buildShell { pkgs = aarch64-linux_pkgs; buildInputs = aarch64LinuxBuildInputs; };
+      devShells.x86_64-linux.default = buildShell { pkgs = x86_64-linux_pkgs; buildInputs = x86_64LinuxBuildInputs; };
 
       formatter.aarch64-darwin = aarch64-darwin_pkgs.nixpkgs-fmt;
       formatter.aarch64-linux = aarch64-linux_pkgs.nixpkgs-fmt;
